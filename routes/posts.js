dcf488f20345d13cf2ca95ebd3b3ef8cb5ca2a96
pavlosp <pavlosp1967@gmail.com>
2019-05-02 22:17:47 -0700

    phone video upload test from iphone

--- routes/posts.js
+++ routes/posts.js
@@ -15,13 +15,13 @@ var storage = multer.diskStorage({
     }
 });
 var imageFilter = function (req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|mp4)$/i)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif|mp4|mov)$/i)) {
         return cb(new Error('Only image files are allowed'), false);
     }
     cb(null, true);
 };
 
-var upload = multer({storage: storage});   //, fileFilter: imageFilter});
+var upload = multer({storage: storage, fileFilter: imageFilter});
 
 var cloudinary = require('cloudinary');
 
