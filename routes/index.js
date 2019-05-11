58fb8d31758ec35f24aa85603108a8460428a6da
pavlosp <pavlosp1967@gmail.com>
2019-05-10 23:34:02 -0700

    update password reset

--- routes/index.js
+++ routes/index.js
@@ -239,7 +239,7 @@ router.post('/forgot', function(req, res, next) {
                 subject: 'Password Reset Info Share',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                     'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                    'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                    'https://' + req.headers.host + '/reset/' + token + '\n\n' +
                     'If you did not request this, please ignore this email and your password will remain unchanged.\n'
             };
             smtpTransport.sendMail(mailOptions, function(err) {
